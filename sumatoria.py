1c2f30459f28ad75b2139f441df12d43bf248b23
TomySpy <jtomas.perez@udea.edu.co>
2024-08-27 20:18:26 -0500

    Update sumatoria.py

--- sumatoria.py
+++ sumatoria.py
@@ -1,14 +1,12 @@
 while True:
     a = int(input("Escriba un numero: "))
+    n = int(input("Escriba la cantidad de sumas: "))
 
     if a != 0: 
         sumatoria = 0
         i = 1
-        while i <= a:
+        while i <= n:
             sumatoria += (1 / a) ** i
             print(f"{sumatoria}.")
             i += 1
-    else:
-        print("Otro que no sea 0.")
-    
-    break 
+   
